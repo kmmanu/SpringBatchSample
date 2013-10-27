8460cd15f6aacf5b9e02fb48b6a87ccd1178439f
kmmanu <manukm07@gmail.com>
2013-10-27 17:43:50 +0530

    Modified with SPEL to get input file. Added missing input file.

--- src/main/java/org/manu/springbatch/EmployeeBatchApp.java
+++ src/main/java/org/manu/springbatch/EmployeeBatchApp.java
@@ -1,26 +1,35 @@
 package org.manu.springbatch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-public class App {
+public class EmployeeBatchApp {
 	public static void main(final String[] args) {
 
 		final GenericXmlApplicationContext context = new GenericXmlApplicationContext();
 		context.load("classpath:applicationContext-batch-sample.xml");
 		context.refresh();
+		// classpath:input/employee.csv
 
-		final JobLauncher jobLauncher = (JobLauncher) context
-				.getBean("jobLauncher");
-		final Job job = (Job) context.getBean("helloWorldJob");
+		final JobLauncher jobLauncher = context.getBean("jobLauncher",
+				JobLauncher.class);
+
+		final Job empJob = context.getBean("importEmployeeJob", Job.class);
+
+		final Map<String, JobParameter> jobParamMap = new HashMap<String, JobParameter>();
+		jobParamMap.put("inputFile", new JobParameter(
+				"classpath:input/employee.csv"));
 
 		try {
-
-			final JobExecution execution = jobLauncher.run(job,
-					new JobParameters());
+			final JobExecution execution = jobLauncher.run(empJob,
+					new JobParameters(jobParamMap));
 			System.out.println("Exit Status : " + execution.getStatus());
 
 		} catch (final Exception e) {
